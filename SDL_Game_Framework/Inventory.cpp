fca9afc7d1da613463a58342696e4930dcc79029
Elit3d <kyle.j.t@live.co.uk>
2015-10-19 11:01:30 +0100

    Added a very basic inventory - Needs expanding on

---
+++ SDL_Game_Framework/Inventory.cpp
@@ -0,0 +1,37 @@
+#include "Inventory.h"
+
+
+Inventory::Inventory()
+{
+}
+
+Inventory::Inventory(char *img, int rows, int columns)
+{
+	m_row = rows;
+	m_column[9] = columns;
+}
+
+Inventory::~Inventory()
+{
+}
+
+bool Inventory::add_item(int item_id)
+{
+	inv_items.push_back(m_item);
+	//possible for loop
+	if (!is_slot_taken) //need to figure out a better way to implement something like this
+	{
+		//add_item(item* m_item) //something like this perhaps?
+		return true;
+	}
+	return false;
+}
+
+bool Inventory::remove_item()
+{
+	//possible loop to erase a specific item
+	inv_items.erase(inv_items.begin());
+}
+
+
+
---
+++ SDL_Game_Framework/Inventory.h
@@ -0,0 +1,31 @@
+#ifndef _INVENTORY_H_
+#define _INVENTORY_H_
+
+#include <vector>
+#include "AWSprite.h"
+#include "Game_Object.h"
+
+const int NUM_GAME_OBJECTS = 20;
+
+class Inventory :
+	public AWSprite
+{
+public:
+	Inventory();
+	Inventory(char *img, int rows, int columns); //uses char img to setup the UI image
+	~Inventory();
+
+	bool remove_item();
+	bool add_item(int item_type);
+
+private:
+	int m_row;
+	int m_column[9];
+	Game_Object m_item;
+	std::vector<Game_Object> inv_items;
+
+	bool is_slot_taken;
+};
+
+#endif
+
