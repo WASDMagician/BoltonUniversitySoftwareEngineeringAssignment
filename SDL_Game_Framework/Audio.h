5a4a2499593cb50f90ac834d111450a02ef2f847
DmasterWang <edward.hawksworth@outlook.com>
2015-10-19 12:51:13 +0100

    Barebones Audio class
    No idea what this is supposed to do so just made an empty class with the
    relevant guards etc

---
+++ SDL_Game_Framework/Audio.h
@@ -0,0 +1,15 @@
+#ifndef _AUDIO_H_
+#define _AUDIO_H_
+#include "AW_Sprite_Interface.h"
+class Audio :
+	public AW_Sprite_Interface
+{
+public:
+	Audio();
+	~Audio();
+	//void PlaySound(std::string sound);
+private:
+
+};
+
+#endif
