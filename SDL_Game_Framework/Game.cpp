f2f573fcb6f2540527128b8d13593612965d7c9d
WASDMagician <WASDMagician@gmail.com>
2015-10-21 17:58:29 +0100

    fixed mistake in last commit.

--- SDL_Game_Framework/Game.cpp
+++ SDL_Game_Framework/Game.cpp
@@ -18,8 +18,6 @@ void Game::Setup()
 	new_player->transform->set_current_position(10, 10);
 
 	m_play_screen_ui = new UI_Play_Screen(1, new_player->get_character_health(), 0, new_player->get_lives(), 0, new_player->get_character_name());
-	m_play_screen_ui->m_flickering = true;
-	m_play_screen_ui->Flicker(10);
 
 
 }
